b2918de3b04445eed860f528b142f07c0ad5731c
kevinsmith <kjsmith.dev@gmail.com>
2017-10-23 15:44:17 -0700

    init project commit

---
+++ SimpleGame/src/test/java/CellTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CellTest
+{
+    @Test
+    public void getName() throws Exception
+    {
+
+    }
+
+    @Test
+    public void setName() throws Exception
+    {
+    }
+
+}
---
+++ SimpleGame/src/test/java/GameBoardTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameBoardTest
+{
+    @Test
+    public void addCell() throws Exception
+    {
+        Cell cell = new Cell();
+        addCell(cell);
+
+        
+    }
+}
